5b191035b03744a607cf89b06e8d9f6208b86d8e
Ebenezer terabo <teraboebenezer@gmail.com>
2024-02-01 23:50:38 +0000

    function support 1

---
+++ file_func1.c
@@ -0,0 +1,103 @@
+#include "monty.h"
+
+/**
+ * tokenize_buf - A function that tokenize the buffer passed to it by the
+ * getline function.
+ *
+ * Return: Nothing
+ */
+
+void tokenize_buf(void)
+{
+	int i = 0;
+	char *buf = NULL, *token = NULL;
+
+	buf = malloc(sizeof(char) * strlen(arg_s->buf) + 1);
+	if (buf == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	strcpy(buf, arg_s->buf);
+	arg_s->num_tokens = 0;
+	token = strtok(buf, DELIM);
+
+	while (token)
+	{
+		arg_s->num_tokens += 1;
+		token = strtok(NULL, DELIM);
+	}
+
+	arg_s->tokens = malloc(sizeof(char *) * (arg_s->num_tokens + 1));
+	strcpy(buf, arg_s->buf);
+	token = strtok(buf, DELIM);
+
+	while (token)
+	{
+		arg_s->tokens[i] = malloc(sizeof(char) * strlen(token) + 1);
+		if (arg_s->tokens[i] == NULL)
+		{
+			fprintf(stderr, "Error: malloc failed\n");
+			exit(EXIT_FAILURE);
+		}
+		strcpy(arg_s->tokens[i], token);
+		printf("%s\n", token);
+		token = strtok(NULL, DELIM);
+		i++;
+	}
+	arg_s->tokens[i] = NULL;
+	free(buf);
+}
+/**
+ * read_and_execute_file - Reads and executes monty bytecocde instructions
+ * from a file.
+ * @filename: the name of the file.
+ *
+ * Return: Nothing
+ */
+
+void read_and_execute_file(FILE *filename)
+{
+	size_t len = 0;
+
+	while (getline(&arg_s->buf, &len, filename) != -1)
+	{
+		arg_s->line_num++;
+		if (arg_s->buf == NULL)
+		{
+			fprintf(stderr, "Error: malloc failed\n");
+			free_tokens();
+			free(arg_s);
+			exit(EXIT_FAILURE);
+		}
+
+		printf(" %d. %s\n", arg_s->line_num, arg_s->buf);
+		tokenize_buf();
+		free_tokens();
+	}
+
+	free(arg_s->buf);
+}
+
+/**
+ * free_tokens - A function that frees tokens in the buffer
+ *
+ * Return: Nothing
+ */
+
+void free_tokens(void)
+{
+	int i = 0;
+
+	if (arg_s->tokens == NULL)
+		return;
+
+	while (i < arg_s->num_tokens)
+	{
+		free(arg_s->tokens[i]);
+		i++;
+	}
+	free(arg_s->tokens);
+	arg_s->tokens = NULL;
+
+}
