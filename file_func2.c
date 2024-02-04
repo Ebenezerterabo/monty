ccaebe5993fbbb0f30299dea73b2c6d4a8dc26e6
Ebenezer terabo <teraboebenezer@gmail.com>
2024-02-04 03:30:12 +0000

    file_func2 added

---
+++ file_func2.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ * free_stack - A function that frees the stack
+ * @head: pointer to the head node
+ *
+ * Return: Nothing
+ */
+
+void free_stack(stack_t **head)
+{
+	stack_t *current = *head;
+	stack_t *next;
+
+	while (current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+
+	*head = NULL;
+}
